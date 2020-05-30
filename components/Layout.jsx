83170136e27e7d3e06c8677c137542c8bc8776e9
jimmysafe <ciaffardini.g@gmail.com>
2020-05-30 18:44:27 +0100

    add navbar responsive

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,25 +2,31 @@ import { useEffect, useState } from 'react'
 import { Client } from '../prismic-configuration'
 import Prismic from "prismic-javascript";
 import Navbar from './Navbar'
+import { useRouter } from 'next/router'
 
 const Layout = ({ children }) => {
     const [navItems, setNavItems] = useState()
+    const router = useRouter()
 
     useEffect(() => {
         if(!navItems){
             const client = Client()
             client
             .query(Prismic.Predicates.at("document.tags", ["nav-link"]))
-            .then(res => setNavItems(res.results))
+            .then(res => {
+                let items = res.results.slice().sort((a, b) => new Date(b.first_publication_date) - new Date(a.first_publication_date))
+                setNavItems(items.reverse())
+            })
             .catch(err => console.log(err))
         }
     }, [])
 
     if(!navItems) return <p>Loading...</p>
 
+    console.log(navItems)
     return (
         <main className='wrapper'> 
-            <Navbar navItems={navItems}/>
+            <Navbar navItems={navItems} query={router.query.page}/>
             {children}
         </main>
     )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { linkResolver, hrefResolver } from '../prismic-configuration'
 
-const Navbar = ({ navItems }) => {
+const Navbar = ({ navItems, query }) => {
+    const [open, setOpen] = useState(false)
+
+    const handleMobileNav = () => {
+        setOpen(!open)
+    }
+
     return (
         <nav>
-            <div className="logo-container">
-
-            </div>
-            <div className="nav-items-container">
-                <ul>
-                    {navItems.map(item => (
-                        <Link as={linkResolver(item)} href={hrefResolver(item)} key={item.id}>
-                            <li>{item.data.page_title[0].text}</li>
-                        </Link>
-                    ))}
-                </ul>
+            <div className="container flex content-between items-center" style={{ height: '87px', position: 'relative' }}>
+                <div className="logo-container">
+                    <img src="/images/logo.jpg" alt="Logo" width='200px'/>
+                </div>
+                <div className={`nav-items-container flex-1 ${open ? 'open' : ''}`}>
+                    <ul className="flex content-end">
+                        {navItems.map(item => (
+                            <Link as={linkResolver(item)} href={hrefResolver(item)} key={item.id}>
+                                <li className={`${query === item.uid ? 'underlined' : ''}`} onClick={() => handleMobileNav()}>
+                                    {item.data.page_title[0].text}
+                                </li>
+                            </Link>
+                        ))}
+                    </ul>
+                </div>
+                <div className="menu-icon-container">
+                    <img 
+                        src={open ? '/images/x.svg' : '/images/menu.svg'} 
+                        alt="Menu Handle"
+                        onClick={() => handleMobileNav()}
+                    />
+                </div>    
             </div>
         </nav>
     )
