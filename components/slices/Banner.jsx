cd4c017e76f4a996ad26758da4416b07b23e7ed4
jimmysafe <ciaffardini.g@gmail.com>
2020-06-12 11:43:19 +0200

    add fix for fonts and banner height format

--- components/slices/Banner.jsx
+++ components/slices/Banner.jsx
@@ -13,10 +13,7 @@ const Banner = (props) => {
             bgImageAlt={slice.primary.bg_image.alt}
             strength={400}
         >
-        <div 
-            className="section-banner"
-            style={{ minHeight: page.uid === 'home' ? '60vh' : '30vh' }}
-        >
+        <div className={`section-banner ${page.uid === 'home' ? 'home-banner' : ' '}`}>
             <div className="container flex content-center items-center column">
                 {heading &&
                     <div className="heading">
