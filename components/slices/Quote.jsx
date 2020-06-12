8343870abac8e390f0b3221e91e9b8facaef44e1
jimmysafe <ciaffardini.g@gmail.com>
2020-06-12 11:54:26 +0200

    add quotes bg

--- components/slices/Quote.jsx
+++ components/slices/Quote.jsx
@@ -1,4 +1,5 @@
 
+import { Parallax } from 'react-parallax';
 
 const Quote = ({ slice }) => {
     const {
@@ -8,19 +9,26 @@ const Quote = ({ slice }) => {
     } = slice.primary
 
     return (
-        <div className="section-quote">
-            <div className="container">
-                <div className="quote">
-                    <h4>{quote_text[0].text}</h4>
-                </div>
-                <div className="author">
-                    <p>{quote_author[0].text}</p>
-                </div>
-                <div className="company">
-                    <p>{author_company[0].text}</p>
+        <Parallax
+            blur={0}
+            bgImage={'/images/quotes.png'}
+            bgImageAlt={'quote'}
+            strength={400}
+        >
+            <div className="section-quote">
+                <div className="container">
+                    <div className="quote">
+                        <h4>{quote_text[0].text}</h4>
+                    </div>
+                    <div className="author">
+                        <p>{quote_author[0].text}</p>
+                    </div>
+                    <div className="company">
+                        <p>{author_company[0].text}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
     )
 }
 
