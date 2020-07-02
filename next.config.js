8f62034a8542911e16fc12e8d3378c14ae02c942
jimmysafe <ciaffardini.g@gmail.com>
2020-07-02 10:49:11 +0200

    test polyfills ie

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const withFonts = require('nextjs-fonts');
 
 module.exports = withSass(withFonts({
     webpack(config, options) {
+        const originalEntry = config.entry;
+
+        config.entry = async () => {
+          const entries = await originalEntry();
+    
+          // add custom polyfills into specific next.js bundle
+          // https://github.com/zeit/next.js/issues/10794
+          const nextPolyfillPath = 'static/runtime/polyfills.js';
+          const nextPolyfills = entries[nextPolyfillPath];
+          if (nextPolyfills) {
+            entries[nextPolyfillPath] = [nextPolyfills, './polyfills.js'];
+          }
+    
+          return entries;
+        };
         return config;
     },
 }));
---
+++ polyfills.js
@@ -0,0 +1,9 @@
+import 'core-js';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import polyfill from '@dotsunited/load-css-polyfill';
+
+polyfill();
+
+
+console.log('MY POLYFILL!');
